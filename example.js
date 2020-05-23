263cfe421937b38ca8fcbb6d40317ee9f1eb0578
Jan Bölsche <regular.gonzales@googlemail.com>
2020-05-23 18:51:19 +0200

    Initial

---
+++ example.js
@@ -0,0 +1,13 @@
+const xxbla = function(xxx, yyy) {
+  vvv()
+  var i;
+  for(i=0; i<=100+200; i++) {
+       xxx(i)
+       yyy(i+100000)
+  } // a comment bla 
+  console.log("reachable")
+  return {a: xxx + yyy, xxx: xxx, z: z};
+    
+    console.log("  xxx unreachable");
+}
+module.exports = xxbla()
---
+++ index.js
@@ -0,0 +1,1 @@
+// terser % -c -m toplevel=true -b semicolons=0,comments=false,ecma=2020,indent_level=2,quote_style=1 | sha256sum
